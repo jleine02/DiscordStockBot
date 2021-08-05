958cc4c60cac9c5e17ee507de65118a04878f413
Jacob <jacob.leiner@gmail.com>
2021-08-05 14:37:37 -0700

    hardcoded token to make sure it works right

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = "ODcyNTcyNzc3NTMyNjUzNjc5.YQr0uQ.0GcVfDcvVLuJn5Pat9XB4DtUi48"
+#TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
