d6e0bbbce29348e7731dd87a5fc80328410fa513
Jacob <jacob.leiner@gmail.com>
2021-08-05 15:18:32 -0700

    reset discord_token because I pushed the hardcoded value to github

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import os
 
 import discord
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
-TOKEN = "ODcyNTcyNzc3NTMyNjUzNjc5.YQr0uQ.0GcVfDcvVLuJn5Pat9XB4DtUi48"
-#TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
