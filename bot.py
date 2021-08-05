0c11d611bd168d79422fe725bd7ec378535894c6
Jacob <jacob.leiner@gmail.com>
2021-08-05 14:41:18 -0700

    still fighting with heroku

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import os
 
 import discord
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 TOKEN = "ODcyNTcyNzc3NTMyNjUzNjc5.YQr0uQ.0GcVfDcvVLuJn5Pat9XB4DtUi48"
 #TOKEN = os.getenv('DISCORD_TOKEN')
 
